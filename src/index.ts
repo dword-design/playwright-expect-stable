0c3a30e2cfe3954a3bd5f5a0b2a47c5ee22c705f
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-31 15:24:29 +0200

    fix: remove options

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import defu from '@dword-design/defu';
 import { expect as base } from '@playwright/test';
 
 export const expect = base.extend({
-  toBeStable: async (locator, options) => {
+  toBeStable: async locator => {
     try {
-      await locator.hover(defu(options, { trial: true }));
+      await locator.hover({ trial: true });
       return { message: () => `Expected element to not be stable`, pass: true };
     } catch (error) {
       return {
